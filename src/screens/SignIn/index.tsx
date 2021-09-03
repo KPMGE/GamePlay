29d0ed7213255e7eb1f4bb49b719bf51b47d29d8
KPMGE <jesuskevin254@gmail.com>
2021-09-03 20:47:29 -0300

    first lines

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { styles } from "./styles";
+
+export const SignIn = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Sign in page</Text>
+    </View>
+  );
+};
